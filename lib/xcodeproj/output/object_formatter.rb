ce7750362787e16a03afad6369b80b03375381bf
Tobias Klonk <tobias.klonk@tklonk.net>
2012-12-07 17:48:40 +0100

    [Output] Project output contains object attributes

--- lib/xcodeproj/output/object_formatter.rb
+++ lib/xcodeproj/output/object_formatter.rb
@@ -12,8 +12,29 @@ module Xcodeproj
         def output_to_formatter (formatter)
           formatter.add_line "#{uuid} = {"
           formatter.indent
+          output_attributes_to_formatter formatter
           formatter.unindent
           formatter.add_line "};"
+        end
+
+        def output_attributes_to_formatter (formatter)
+          formatter.add_value_for_key self.isa, "isa"
+          self.class.attributes.sort {|a,b| a.plist_name <=> b.plist_name}.each do |attribute|
+            case attribute.type
+              when :simple
+                formatter.add_value_for_key attribute.get_value(self), attribute.plist_name
+              when :to_one
+                formatter.add_value_for_key attribute.get_value(self), attribute.plist_name
+              when :to_many
+                formatter.add_line "#{attribute.plist_name} = ("
+                formatter.indent
+                attribute.get_value(self).each do |object|
+                  formatter.add_line "#{object.uuid} /* #{object.reference_comment} */,"
+                end
+                formatter.unindent
+                formatter.add_line ");"
+            end
+          end
         end
 
       end
