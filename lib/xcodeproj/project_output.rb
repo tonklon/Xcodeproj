ecd0555052d53fc113507a5df13ba5453c258c0a
Tobias Klonk <tobias.klonk@tklonk.net>
2012-12-07 15:32:16 +0100

    [Output] Project files include object section comments

--- lib/xcodeproj/project_output.rb
+++ lib/xcodeproj/project_output.rb
@@ -11,10 +11,25 @@ module Xcodeproj
       formatter.add_value_for_key archive_version, "archiveVersion"
       formatter.add_value_for_key classes, "classes"
       formatter.add_value_for_key object_version, "objectVersion"
+
+      add_objects_to_formatter formatter
+
       formatter.add_value_for_key root_object, "rootObject"
       formatter.unindent
       formatter.formatted_file
     end
+
+    def add_objects_to_formatter (formatter)
+      objects_by_isa.keys.sort.each do |isa_group|
+        formatter.add_unindented_line "/* Begin #{isa_group} section */"
+        formatter.add_unindented_line "/* End #{isa_group} section */"
+      end
+    end
+
+    def objects_by_isa
+      objects.group_by { |object| object.isa}
+    end
+
   end
 
 end
--- spec/project_output_spec.rb
+++ spec/project_output_spec.rb
@@ -28,6 +28,63 @@ module ProjectSpecs
       it "output the root object" do
         @project.to_pbxproj.should.include "rootObject = E525238316245A900012E2BA /* Project object */;"
       end
+
+      it "output all object groups" do
+        output = @project.to_pbxproj
+        output.should.include "/* Begin PBXAggregateTarget section */"
+        output.should.include "/* End PBXAggregateTarget section */"
+        output.should.include "/* Begin PBXBuildFile section */"
+        output.should.include "/* End PBXBuildFile section */"
+        output.should.include "/* Begin PBXBuildRule section */"
+        output.should.include "/* End PBXBuildRule section */"
+        output.should.include "/* Begin PBXContainerItemProxy section */"
+        output.should.include "/* End PBXContainerItemProxy section */"
+        output.should.include "/* Begin PBXCopyFilesBuildPhase section */"
+        output.should.include "/* End PBXCopyFilesBuildPhase section */"
+        output.should.include "/* Begin PBXFileReference section */"
+        output.should.include "/* End PBXFileReference section */"
+        output.should.include "/* Begin PBXFrameworksBuildPhase section */"
+        output.should.include "/* End PBXFrameworksBuildPhase section */"
+        output.should.include "/* Begin PBXGroup section */"
+        output.should.include "/* End PBXGroup section */"
+        output.should.include "/* Begin PBXHeadersBuildPhase section */"
+        output.should.include "/* End PBXHeadersBuildPhase section */"
+        output.should.include "/* Begin PBXLegacyTarget section */"
+        output.should.include "/* End PBXLegacyTarget section */"
+        output.should.include "/* Begin PBXNativeTarget section */"
+        output.should.include "/* End PBXNativeTarget section */"
+        output.should.include "/* Begin PBXProject section */"
+        output.should.include "/* End PBXProject section */"
+        output.should.include "/* Begin PBXReferenceProxy section */"
+        output.should.include "/* End PBXReferenceProxy section */"
+        output.should.include "/* Begin PBXResourcesBuildPhase section */"
+        output.should.include "/* End PBXResourcesBuildPhase section */"
+        output.should.include "/* Begin PBXRezBuildPhase section */"
+        output.should.include "/* End PBXRezBuildPhase section */"
+        output.should.include "/* Begin PBXShellScriptBuildPhase section */"
+        output.should.include "/* End PBXShellScriptBuildPhase section */"
+        output.should.include "/* Begin PBXSourcesBuildPhase section */"
+        output.should.include "/* End PBXSourcesBuildPhase section */"
+        output.should.include "/* Begin PBXTargetDependency section */"
+        output.should.include "/* End PBXTargetDependency section */"
+        output.should.include "/* Begin PBXVariantGroup section */"
+        output.should.include "/* End PBXVariantGroup section */"
+        output.should.include "/* Begin XCBuildConfiguration section */"
+        output.should.include "/* End XCBuildConfiguration section */"
+        output.should.include "/* Begin XCConfigurationList section */"
+        output.should.include "/* End XCConfigurationList section */"
+        output.should.include "/* Begin XCVersionGroup section */"
+        output.should.include "/* End XCVersionGroup section */"
+      end
+
+      it "output the object groups in alphabetical order" do
+        output = @project.to_pbxproj
+        output.index('/* Begin PBXAggregateTarget section */').should < output.index('/* Begin PBXBuildFile section */')
+        output.index('/* End PBXBuildFile section */').should < output.index('/* End PBXBuildRule section */')
+        output.index('/* End PBXLegacyTarget section */').should < output.index('/* Begin PBXNativeTarget section */')
+        output.index('/* End PBXSourcesBuildPhase section */').should < output.index('/* Begin PBXTargetDependency section */')
+        output.index('/* End XCConfigurationList section */').should < output.index('/* Begin XCVersionGroup section */')
+      end
     end
 
   end
