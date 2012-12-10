8d8b4f7aed52051790b7d5961ac944337c236992
Tobias Klonk <tobias.klonk@tklonk.net>
2012-12-10 16:20:00 +0100

    [Output] Added additional empty line above object groups

--- lib/xcodeproj/project_output.rb
+++ lib/xcodeproj/project_output.rb
@@ -27,6 +27,7 @@ module Xcodeproj
 
     def add_objects_to_formatter (formatter)
       objects_by_isa.keys.sort.each do |isa_group|
+        formatter.add_unindented_line ""
         formatter.add_unindented_line "/* Begin #{isa_group} section */"
         objects_in_section = objects_by_isa[isa_group]
         objects_in_section.sort!{|a,b| a.uuid <=> b.uuid}
